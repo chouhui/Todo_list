62ac0cff5bf965f3e3d7555ca2d78e2d3ff6c5c8
DESKTOP-BD78M9D\bigbig <zhidao666@gmail.com>
2017-09-05 21:19:23 +0800

    对请求的数据进行处理

---
+++ models/todo.py
@@ -0,0 +1,46 @@
+import time
+
+from models import Model
+from utils import formatted_time
+
+
+class Todo(Model):
+    def __init__(self, form):
+        super().__init__(form)
+        self.task = form.get('task', '')
+        self.completed = False
+        self.user_id = form.get('user_id', -1)
+        self.created_time = form.get('created_time')
+        self.updated_time = form.get('updated_time')
+
+    @classmethod
+    def new(cls, form):
+        m = super().new(form)
+        t = int(time.time())
+        m.created_time = t
+        m.updated_time = t
+        m.save()
+        return m
+
+    @classmethod
+    def update(cls, id, form):
+        t = cls.find(id)
+        valid_names = [
+            'task',
+        ]
+        for key in form:
+            # 这里只更新需要更新的东西
+            if key in valid_names:
+                setattr(t, key, form[key])
+        t.updated_time = int(time.time())
+        t.save()
+        return t
+
+    def is_owner(self, id):
+        return self.user_id == id
+
+    def formatted_created_time(self):
+        return formatted_time(self.created_time)
+
+    def formatted_updated_time(self):
+        return formatted_time(self.updated_time)
